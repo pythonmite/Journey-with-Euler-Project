dcb50839d60ebea08e726b28655c402590720529
pythonmite <aman.pythonmite@gmail.com>
2019-01-13 23:40:05 +0530

    Solution for Multiples of 3 and 5 (Problem 1) of Euler.net.

--- Multiples of 3 and 5/solution.py
+++ Multiples of 3 and 5/solution.py
@@ -1,8 +1,12 @@
 """
-
     Problem Statement :
         If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
         The sum of these multiples is 23.
 
         Find the sum of all the multiples of 3 or 5 below 1000.
 """
+
+# Using list comprehension and 'sum' function
+
+result = sum([x for x in range(1000) if x%3 == 0 or x%5 == 0 ])
+print(result)

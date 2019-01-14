c5baa4eb38438f1b0f6c86ee65b9d46794f34a71
pythonmite <aman.pythonmite@gmail.com>
2019-01-14 15:01:34 +0530

    Solution for Largest Palindrome Product (Problem 4) of Euler.net.

--- Largest palindrome product/solution.py
+++ Largest palindrome product/solution.py
@@ -6,7 +6,7 @@
         Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
-# first we will calculate all the palindrome between 100 to 999 since
+# first we will calculate all the palindrome between 100 to 999 since they want the largest palindrome product for 3-digit number
 
 palidrome_product_list = [i*j for i in range(1000, 100, -1) for j in range(1000, 100, -1) if str(i*j)[::] ==
                           str(i*j)[::-1]]

32bc22ae03d40d625ddab8af6d3763d98c3d2f27
pythonmite <aman.pythonmite@gmail.com>
2019-01-14 18:26:15 +0530

    Solution for Power Didgit Sum (Problem 16) of Euler.net.

---
+++ Power digit sum/solution.py
@@ -0,0 +1,19 @@
+"""
+    Problem Statement :
+        2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+        What is the sum of the digits of the number 2**1000?
+
+"""
+
+# use of type casting
+a = str(2**1000)
+
+# Use 'map' function to convert str into a list of individual numbers
+a = list(map(int, a))
+
+# Use sum function to calculate the sum of the list
+result = sum(a)
+print(result)
+# Answer 1366
+
